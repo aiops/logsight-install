fedb8144a633ad2ab958b879688c4dc70263177c
Sasho Nedelkoski <sashonedelkoski@gmail.com>
2022-04-25 09:57:45 +0200

    V1.0.1 (#17)
    * add verification query size parameter (#11)
    
    * adding password check for ES and postgres
    
    * adjusted password requirements and formatting
    
    * fixed installation script. added license to be referenced by installation script
    
    * fixed prompting
    
    * Uninstall fix (#14)
    
    * fixed uninstall script checking for docker compose command
    
    * fixed uninstall script
    
    * pw requirment text refinement
    
    * moved eula, changes dir name (#15)
    
    * Readme (#16)
    
    * fixed uninstall script checking for docker compose command
    
    * fixed uninstall script
    
    * refined readme
    
    * refined readme
    
    * upgrade for v1.0.1
    
    Co-authored-by: Alexander Acker <alexander.acker@tu-berlin.de>
    Co-authored-by: pilijevski <petar.ilijevski@gmail.com>

--- docker-compose/uninstall.sh
+++ docker-compose/uninstall.sh
@@ -2,10 +2,37 @@
 # Copyright 2021-2022 logsight.ai
 
 home="$(cd -- "$(dirname -- "$0")"; pwd)"
+set -e
+
+check_docker_compose() {
+    # check if docker-compose is installed
+    if [ -z "$(command -v docker-compose)" ]; then
+        # if not check if docker compose is working
+        docker compose version > /dev/null 2>&1
+        if [ $? -eq 0 ]; then
+            echo "docker compose"
+        else
+            echo "Logsight.ai requires docker-compose to be uninstalled. Please install it on your system and re-run the uninstallation."
+            exit 1
+        fi
+    else
+        echo "docker-compose"
+    fi
+}
+
+DOCKER_COMPOSE_CMD="$(check_docker_compose)"
+
+export ELASTICSEARCH_PASSWORD=""
+export POSTGRES_PASSWORD=""
+export ACCEPT_LOGSIGHT_LICENSE=""
+
 cd "$home/docker-compose"
+$DOCKER_COMPOSE_CMD down -v --rmi 'all'
 
-docker-compose rm --force --stop -v
-docker volume rm --force $(docker volume ls -f name=logsight -q) > /dev/null 2>&1
-docker network rm $(docker network ls -f name=logsight -q) > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+    echo "Uninstall logsight successful."
+else
+    echo "Uninstall logsight went wrong."
+fi
 
-echo "Uninstall logsight successful"
+cd "$home" 
