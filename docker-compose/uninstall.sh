1a3ac7f00b5765ee211cfeab3c46d07a5d3bc8b8
Alexander Acker <alexander.acker@tu-berlin.de>
2022-04-26 13:25:02 +0200

    added update scripts, restructured functions

--- docker-compose/uninstall.sh
+++ docker-compose/uninstall.sh
@@ -4,22 +4,11 @@
 home="$(cd -- "$(dirname -- "$0")"; pwd)"
 set -e
 
-check_docker_compose() {
-    # check if docker-compose is installed
-    if [ -z "$(command -v docker-compose)" ]; then
-        # if not check if docker compose is working
-        docker compose version > /dev/null 2>&1
-        if [ $? -eq 0 ]; then
-            echo "docker compose"
-        else
-            echo "Logsight.ai requires docker-compose to be uninstalled. Please install it on your system and re-run the uninstallation."
-            exit 1
-        fi
-    else
-        echo "docker-compose"
-    fi
-}
+. "$home/bashlib/utils.sh"
 
+# Check if the preconditions to install logsight are met
+check_docker
+# Check docker compose and set the docker compose command variable
 DOCKER_COMPOSE_CMD="$(check_docker_compose)"
 
 export ELASTICSEARCH_PASSWORD=""
